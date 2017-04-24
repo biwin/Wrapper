a5b4d49d09dbda43c9cab7f64ac8c7773b3faa7f
biwin <biwin007@gmail.com>
2017-04-24 23:03:04 +0530

    adds documentation

--- wrapper/locu.py
+++ wrapper/locu.py
@@ -9,6 +9,7 @@ class LocuAPIWrapper(BaseWrapper):
         self.endpoint = 'https://api.locu.com/v2/venue/search'
 
     def search_restaurant(self, **kwargs):
+        """ search api v2 """
         locality = kwargs.get('locality', None)
         location = kwargs.get('location', None)
         locu_id = kwargs.get('locu_id', None)
@@ -44,10 +45,12 @@ class LocuAPIWrapper(BaseWrapper):
         return self._post_endpoint(self.endpoint)
 
     def get_restaurant_details(self, venue_id):
+        """ get the deatils of the given venu_id """
         endpoint = 'http://api.locu.com/v1_0/venue/%s/?api_key=%s' % (venue_id, self.api_key)
         return self._get_endpoint(endpoint)
 
     def get_restuarants_with_menu(self, query):
+        """ get the list of restaurants with menuus that match the given query """
         endpoint = 'http://api.locu.com/v1_0/venue/search/?api_key=%s&has_menu=true&%s' % (self.api_key, query)
         return self._get_endpoint(endpoint)
 
