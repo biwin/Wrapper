4c759f5690de9874a20a2554887eb84708b76284
biwin <biwin007@gmail.com>
2017-04-24 22:07:48 +0530

    adds wrapper

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Wrapper',
+    packages=['wrapper'],
+    version='0.1',
+    description='Random API Wrappers',
+    author='Biwin John',
+    author_email='biwinjohn@gmail.com',
+    url='https://github.com/biwin/wrapper',
+    download_url='https://github.com/biwin/wrapper/archive/0.1.tar.gz',
+    keywords=['api', 'python'],
+    classifiers=[],
+)
