6290dea6799728be356116e10055d2baa3678b5a
biwin <biwin007@gmail.com>
2017-04-24 22:38:03 +0530

    adds requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='Wrapper',
     packages=['wrapper'],
     version='0.1',
+    license="GPL",
     description='Random API Wrappers',
     author='Biwin John',
     author_email='biwinjohn@gmail.com',
     url='https://github.com/biwin/wrapper',
     download_url='https://github.com/biwin/wrapper/archive/0.1.tar.gz',
+    install_requires=[],
+    requires=['requests'],
     keywords=['api', 'python'],
     classifiers=[],
 )
